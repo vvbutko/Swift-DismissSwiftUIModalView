203d5d6e9f994dd06ee0d0c0024a91ae8cc93559
Vladimir Butko <vvbutko@gmail.com>
2021-10-31 17:14:37 +0100

    Update files

--- DismissSwiftUIModalView/SettingsScreen.swift
+++ DismissSwiftUIModalView/SettingsScreen.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct SettingsScreen: View {
+    var dismissAction: (() -> Void)
+    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            
+            VStack(spacing: 16) {
+                Button("**UIKit** - using passed action from UIViewController") {
+                    dismissAction()
+                }
+                
+                Button("**iOS 13** - using SwiftUI environment value `presentationMode`") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                
+                Button("**iOS 15** - using SwiftUI environment value `dismiss`") {
+                    dismiss()
+                }
+                
+                Spacer()
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        if #available(iOS 15.0, *) {
+                            dismiss()
+                        } else {
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 struct SettingsScreen_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsScreen()
+        SettingsScreen(dismissAction: {
+            print("dismiss tapped")
+        })
     }
 }
--- DismissSwiftUIModalView/ViewController.swift
+++ DismissSwiftUIModalView/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViewController()
     }
 
-
+    private func configureViewController() {
+        title = "Dismiss SwiftUI Modal View"
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        let presentSettingsAction = UIAction { [unowned self] _ in
+            self.presentSwiftUISettingsScreen()
+        }
+        
+        let settingsButton = UIBarButtonItem(
+            image: UIImage(systemName: "gear"),
+            primaryAction: presentSettingsAction
+        )
+        
+        navigationItem.rightBarButtonItem = settingsButton
+    }
+    
+    private func presentSwiftUISettingsScreen() {
+        let settingsScreen = SettingsScreen(
+            dismissAction: { [unowned self] in
+                self.dismiss(animated: true, completion: nil)
+            }
+        )
+        
+        let hostingController = UIHostingController(rootView: settingsScreen)
+        present(hostingController, animated: true, completion: nil)
+    }
 }
 
