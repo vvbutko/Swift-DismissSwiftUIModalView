e2e903186283c9d71d9f63bcba1b71d3b27f2549
Vladimir Butko <vvbutko@gmail.com>
2021-10-31 17:13:01 +0100

    Add .gitignore file

---
+++ DismissSwiftUIModalView/SettingsScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsScreen.swift
+//  DismissSwiftUIModalView
+//
+//  Created by Vladimir Butko on 03/10/2021.
+//
+
+import SwiftUI
+
+struct SettingsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsScreen()
+    }
+}
